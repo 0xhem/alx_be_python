fd124e2596d6665813fc3e56b62edf8234faadba
0xhem <osensidiq@gmail.com>
2025-06-01 23:56:07 +0100

    Updated the match case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,23 +1,24 @@
 #Requesting number input from users
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+#Selected operations
 add = num1+num2
 sub = num1-num2
 multi = num1*num2
 
 #Asking the user for operation they'd like to perform
-operation = input("Choose the operation (+, -, *, /): " )
-if operation == "+":
-    result = "addition"
-elif operation == "-":
-    result = "substraction"
-elif operation == "*":
-    result = "multiplication"
-elif operation == "/":
-    result = "division"
-else:
-    print("Cannot divide by zero")
-match result:
+input_prompt = input("Choose the operation (+, -, *, /): " )
+if  input_prompt == "+":
+    operation = "addition"
+elif input_prompt == "-":
+    operation = "substraction"
+elif input_prompt == "*":
+    operation = "multiplication"
+elif input_prompt == "/":
+    operation = "division"
+
+match operation:
     case "addition":
         print("The result is " + str(add) + ".")
     case "substraction":
@@ -25,7 +26,7 @@ match result:
     case "multiplication":
         print("The result is " + str(multi) + ".")
     case "division":
-        if num2 == 0:
+        if num2 == 0: #In a case if the user wants to divide by zero; to prevent runtime error
             print("Can't divide by zero.")
         else:
             print("The result is " + str(num1/num2) + ".")
