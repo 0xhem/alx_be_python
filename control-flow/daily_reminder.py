4aff421beb6d944c01859e7f14ffb8d0dea5ef0c
0xhem <osensidiq@gmail.com>
2025-06-10 14:05:32 +0100

    Updated daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower
-time_bound = input("Is it time bound? (yes/no): ").lower
+time_bound = input("Is it time-bound? (yes/no): ").lower
 
 while priority not in ("high", "medium", "low"):                # To make sure the right argument is passed
     print("Please enter a valid priority: " )
